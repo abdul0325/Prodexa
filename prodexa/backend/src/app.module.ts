914e0470ebbf06b4daceb01dfb41d399b28ba28f
Abdul Rehman <habdul2266@gmail.com>
2026-05-20 21:26:12 +0500

    test again webhook

--- prodexa/backend/src/app.module.ts
+++ prodexa/backend/src/app.module.ts
@@ -2,22 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
+import { UserModule } from './prisma/users/user.module';
+import { AuthModule } from './prisma/auth/auth.module';
 import { GithubModule } from './github/github.module';
-import { ProjectModule } from './project/project.module';
+import { ProjectModule } from './prisma/projects/project.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { DeveloperAnalyticsModule } from './developer-analytics/developer-analytics.module';
 import { IntelligenceModule } from './intelligence/intelligence.module';
-import { AnalyticsQueueModule } from './analytics-queue/analytics-queue.module';
+import { AnalyticsQueueModule } from './analytics/analytics.module';
 import { BullModule } from '@nestjs/bullmq';
-import { DashboardModule } from './dashboard/dashboard.module';
+import { DashboardModule } from './presentation/presentation.module';
 import { MLDataModule } from './ml-data/ml-data.module';
 import { MLModule } from './ml/ml.module';
 import { AdminModule } from './admin/admin.module';
-import { NotificationsModule } from './notifications/notifications.module';
+import { NotificationsModule } from './prisma/notifications/notifications.module';
 import { GatewayModule } from './gateway/gateway.module';
 import { RedisModule } from '@nestjs-modules/ioredis';
+import { GithubWebhooksModule } from './github/webhooks/github-webhooks.module';
 
 @Module({
   imports: [
@@ -49,7 +50,8 @@ import { RedisModule } from '@nestjs-modules/ioredis';
     MLModule,
     AdminModule,
     NotificationsModule,
-    GatewayModule, // NEW: WebSocket real-time
+    GatewayModule,
+    GithubWebhooksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
