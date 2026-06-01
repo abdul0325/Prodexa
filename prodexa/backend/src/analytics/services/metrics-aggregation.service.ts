f576171ec51a4856572d43d5d739895c110395b6
Abdul Rehman <habdul2266@gmail.com>
2026-06-01 19:56:58 +0500

    second test

--- prodexa/backend/src/analytics/services/metrics-aggregation.service.ts
+++ prodexa/backend/src/analytics/services/metrics-aggregation.service.ts
@@ -35,7 +35,7 @@ export class MetricsAggregationService {
         normalizedEvent: any,
     ) {
         console.log(
-            'AGGREGATING SNAPSHOTS',
+            'AGGREGATING SNAPSHOTS IDs',
             {
                 projectId: normalizedEvent.projectId,
                 repositoryId: normalizedEvent.repositoryId,
